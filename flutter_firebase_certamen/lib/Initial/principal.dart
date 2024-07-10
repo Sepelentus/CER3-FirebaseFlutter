bc38a6f8f01ec41d95034afd7ce9562724eb8976
Jordan Navarrete <jordan.navarrete@usm.cl>
2024-07-10 18:23:34 -0400

    Changes

--- flutter_firebase_certamen/lib/Initial/principal.dart
+++ flutter_firebase_certamen/lib/Initial/principal.dart
@@ -7,6 +7,7 @@ class Principalpage extends StatelessWidget {
   const Principalpage({super.key});
 
   @override
+<<<<<<< Updated upstream
   Widget build(BuildContext context){
     return DefaultTabController(
       length: 2,
@@ -29,8 +30,29 @@ class Principalpage extends StatelessWidget {
         ),
         body: const TabBarView(
           children: [JugadoresPage(), Copas()],
+=======
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 3, child: Scaffold(appBar: AppBar(
+      title: Container(
+        height: 80,
+        child: Image.asset('assets/images/cc.png',
+          fit: BoxFit.contain,
+>>>>>>> Stashed changes
         ),
       ),
+      bottom: TabBar(
+        labelColor: Colors.black,
+        unselectedLabelColor: Colors.white,
+        indicatorColor: Colors.amber,
+        tabs: [
+          Tab(icon: Icon(Icons.home), text: 'Inicio'),
+          Tab(icon: Icon(Icons.search), text: 'Buscar'),
+          Tab(icon: Icon(Icons.person), text: 'Perfil'),
+        ],
+    )
+    ),
+    body: TabBarView(children: [Campeonatos(), Jugadores(), otros()],),
+    )
     );
   }
 }
